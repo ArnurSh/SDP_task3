ea53f63fbc6b0858ddc562459538fb93fa15cf6d
KMD <71997429+ArnurSh@users.noreply.github.com>
2024-10-15 15:39:30 +0500

    Update ShippingService.java

--- ShippingService.java
+++ ShippingService.java
@@ -1,7 +1,7 @@
 public class ShippingService {
     public int calculateShipping(String productName) {
         System.out.println("Shipping calculated for " + productName + ".");
-        return 10; // Фиксированная ставка доставки
+        return 10;
     }
 
     public void shipProduct(String productName) {
